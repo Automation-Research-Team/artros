65b24dea158858828da7e45859cc6c3b1b8e4d66
Toshio UESHIBA <t.ueshiba@aist.go.jp>
2021-07-12 17:23:39 +0900

    Make it possible to multiple cameras and multiplexer are launched as nodelets

--- aist_camera_multiplexer/scripts/select_realsense_camera.py
+++ aist_camera_multiplexer/scripts/select_realsense_camera.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
 
     camera_name = sys.argv[1]
 
-    rospy.init_node('~')
+    rospy.init_node('select_realsesne_camera')
     multiplexer = RealSenseMultiplexerClient('camera_multiplexer')
     multiplexer.activate_camera(camera_name)
