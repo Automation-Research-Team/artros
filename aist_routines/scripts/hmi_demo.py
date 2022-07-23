3ca06163a48e4d41ab9fe0667eec4815e7556fab
Toshio UESHBIA <t.ueshiba@aist.go.jp>
2022-07-23 12:21:37 +0900

    WIP: add server and client for human-machine interface demonstration

---
+++ aist_routines/scripts/hmi_demo.py
@@ -0,0 +1,295 @@
+#!/usr/bin/env python
+#
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2021, National Institute of Advanced Industrial Science and Technology (AIST)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of National Institute of Advanced Industrial
+#    Science and Technology (AIST) nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Toshio Ueshiba
+#
+import rospy, collections
+from math                     import pi, radians, degrees
+from geometry_msgs.msg        import QuaternionStamped, PoseStamped
+from aist_routines            import AISTBaseRoutines
+from aist_routines.msg        import pickOrPlaceResult, sweepResult
+from finger_pointing_msgs.msg import (RequestHelpAction, RequestHelpGoal,
+                                      RequestHelpResult, request_help)
+from actionlib                import SimpleActionClient
+from actionlib_msgs.msg       import GoalStatus
+
+######################################################################
+#  class HMIDemoRoutines                                             #
+######################################################################
+class HMIDemoRoutines(AISTBaseRoutines):
+    """Implements HMI routines for aist robot system."""
+
+    def __init__(self, server='hmi_server'):
+        super(HMIDemoRoutines, self).__init__()
+
+        self._bin_props         = rospy.get_param('~bin_props')
+        self._part_props        = rospy.get_param('~part_props')
+        self._former_robot_name = None
+        self._fail_poses        = []
+        self._request_help      = SimpleActionClient(server + '/request_help',
+                                                     RequestHelpAction)
+        self._request_help.wait_for_server()
+
+    @property
+    def nbins(self):
+        return len(self._bin_props)
+
+    @property
+    def former_robot_name(self):
+        return self._former_robot_name
+
+    ###----- main procedure
+    def run(self):
+        self.go_to_named_pose('all_bots', 'back')
+        for bin_id in self._binprops.keys():
+            if rospy.is_shutdown():
+                break
+            self.attempt_bin(bin_id, 1)
+        self.go_to_named_pose('all_bots', 'home')
+
+    def demo(self, bin_id, max_attempts=1):
+        while kitting.attempt_bin(bin_id, 5):
+            pass
+        self.go_to_named_pose(self.former_robot_name, 'home')
+
+    def search(self, bin_id, max_slant=pi/4):
+        bin_props  = self._bin_props[bin_id]
+        part_props = self._part_props[bin_props['part_id']]
+        self.graspability_send_goal(part_props['robot_name'],
+                                    bin_props['part_id'], bin_props['mask_id'])
+        self.camera(part_props['camera_name']).trigger_frame()
+
+        orientation = QuaternionStamped()
+        orientation.header.frame_id = self.reference_frame
+        orientation.quaternion.x = 0
+        orientation.quaternion.y = 0
+        orientation.quaternion.z = 0
+        orientation.quaternion.w = 1
+        return self.graspability_wait_for_result(orientation, max_slant)
+
+    def attempt_bin(self, bin_id, max_attempts=5):
+        bin_props  = self._bin_props[bin_id]
+        part_id    = bin_props['part_id']
+        part_props = self._part_props[part_id]
+        robot_name = part_props['robot_name']
+
+        # If using a different robot from the former, move it back to home.
+        if self._former_robot_name is not None and \
+           self._former_robot_name != robot_name:
+            self.go_to_named_pose(self._former_robot_name, 'back')
+        self._former_robot_name = robot_name
+
+        # Move to 0.15m above the bin if the camera is mounted on the robot.
+        if self._is_eye_on_hand(robot_name, part_props['camera_name']):
+            self.go_to_frame(robot_name, bin_props['name'], (0, 0, 0.15))
+
+        # Search for graspabilities.
+        poses, _ = self.search(bin_id)
+
+        # Attempt to pick the item.
+        nattempts = 0
+        for i, p in enumerate(poses.poses):
+            if nattempts == max_attempts:
+                break
+
+            pose = PoseStamped(poses.header, p)
+            if self._is_close_to_fail_poses(pose):
+                continue
+
+            result = self.pick(robot_name, pose, part_id)
+            if result == pickOrPlaceResult.SUCCESS:
+                result = self.place_at_frame(robot_name,
+                                             part_props['destination'],
+                                             part_id)
+                return result == pickOrPlaceResult.SUCCESS
+            elif result == pickOrPlaceResult.MOVE_FAILURE or \
+                 result == pickOrPlaceResult.APPROACH_FAILURE:
+                self._fail_poses.append(pose)
+            elif result == pickOrPlaceResult.DEPARTURE_FAILURE:
+                self.release(robot_name)
+                raise RuntimeError('Failed to depart from pick/place pose')
+            elif result == pickOrPlaceResult.GRASP_FAILURE:
+                goal = RequestHelpGoal()
+                goal.request.robot_name = robot_name
+                goal.request.item_id    = part_id
+                goal.request.pose       = pose
+                goal.request.request    = request_help.SWEEP_DIR_REQ
+                goal.request.message    = 'Picking failed! Please specify sweep direction.'
+                if self._request_help.send_goal_and_wait(goal) \
+                   is not GoalStatus.SUCCEEDED:
+                    rospy.logerr('(hmi_demo) no response received to the request')
+                    self._fail_poses.append(pose)
+                    nattempts += 1
+                    continue
+                response = self_request_help.get_result().response
+                if response.pointing_state == pointing.SWEEP_RES:
+                    rospy.loginfo('(hmi_demo) given sweep direction.')
+                    self.sweep_bin(bin_id,
+                                   self._compute_sweep_dir(response.header,
+                                                           response.finger_pos,
+                                                           response.finger_dir))
+                elif response.pointing_state == pointing.RECAPTURE_RES:
+                    rospy.loginfo('(hmi_demo) commanded recapture.')
+                    return False
+                else:
+                    raise RuntimeError('received unknown command!')
+
+            self.release(robot_name)
+
+        return False
+
+    def sweep_bin(self, bin_id):
+        bin_props  = self._bin_props[bin_id]
+        part_id    = bin_props['part_id']
+        part_props = self._part_props[part_id]
+        robot_name = part_props['robot_name']
+
+        # If using a different robot from the former, move it back to home.
+        if self._former_robot_name is not None and \
+           self._former_robot_name != robot_name:
+            self.go_to_named_pose(self._former_robot_name, 'back')
+        self._former_robot_name = robot_name
+
+        # Move to 0.15m above the bin if the camera is mounted on the robot.
+        if self._is_eye_on_hand(robot_name, part_props['camera_name']):
+            self.go_to_frame(robot_name, bin_props['name'], (0, 0, 0.15))
+
+        # Search for graspabilities.
+        poses, _ = self.search(bin_id, 0.0)
+
+        # Attempt to sweep the item.
+        p = poses.poses[0]
+        pose = PoseStamped(poses.header, p)
+
+        result = self.sweep(robot_name, pose, part_id)
+        return result == sweepResult.SUCCESS
+
+    def clear_fail_poses(self):
+        self._fail_poses = []
+
+    def _is_eye_on_hand(self, robot_name, camera_name):
+        return camera_name == robot_name + '_camera'
+
+    def _is_close_to_fail_poses(self, pose):
+        for fail_pose in self._fail_poses:
+            if self._is_close_to_fail_pose(pose, fail_pose, 0.005):
+                return True
+        return False
+
+    def _is_close_to_fail_pose(self, pose, fail_pose, tolerance):
+        position      = pose.pose.position
+        fail_position = fail_pose.pose.position
+        if abs(position.x - fail_position.x) > tolerance or \
+           abs(position.y - fail_position.y) > tolerance or \
+           abs(position.z - fail_position.z) > tolerance:
+            return False
+        return True
+
+    def _compute_sweep_dir(self, header, finger_pos, finger_dir):
+        pass
+
+
+if __name__ == '__main__':
+
+    rospy.init_node('hmi_demo', anonymous=True)
+
+    with HMIDemoRoutines() as hmi_demo:
+        while not rospy.is_shutdown():
+            print('============ hmi_ procedures ============ ')
+            print('  b: Create a backgroud image')
+            print('  m: Create a mask image')
+            print('  s: Search graspabilities')
+            print('  a: Attempt to pick and place')
+            print('  A: Repeat attempts to pick and place')
+            print('  w: sWeep')
+            print('  d: Perform small demo')
+            print('  k: Do hmi_demo task')
+            print('  g: Grasp')
+            print('  r: Release')
+            print('  H: Move all robots to home')
+            print('  B: Move all robots to back')
+            print('  q: Quit')
+
+            try:
+                key = raw_input('>> ')
+                if key == 'q':
+                    if hmi_demo.former_robot_name is not None:
+                        hmi_demo.go_to_named_pose(hmi_demo.former_robot_name,
+                                                 'home')
+                    break
+                elif key == 'H':
+                    hmi_demo.go_to_named_pose('all_bots', 'home')
+                elif key == 'B':
+                    hmi_demo.go_to_named_pose('all_bots', 'back')
+                elif key == 'b':
+                    hmi_demo.create_background_image('a_phoxi_m_camera')
+                elif key == 'm':
+                    hmi_demo.create_mask_image('a_phoxi_m_camera',
+                                              hmi_demo.nbins)
+                elif key == 's':
+                    bin_id = 'bin_' + raw_input('  bin id? ')
+                    hmi_demo.search(bin_id)
+                elif key == 'a':
+                    bin_id = 'bin_' + raw_input('  bin id? ')
+                    hmi_demo.clear_fail_poses()
+                    hmi_demo.attempt_bin(bin_id, 5)
+                    hmi_demo.go_to_named_pose(hmi_demo.former_robot_name, 'home')
+                elif key == 'A':
+                    bin_id = 'bin_' + raw_input('  bin id? ')
+                    hmi_demo.clear_fail_poses()
+                    while hmi_demo.attempt_bin(bin_id, 5):
+                        pass
+                    hmi_demo.go_to_named_pose(hmi_demo.former_robot_name, 'home')
+                elif key == 'c':
+                    self.pick_or_place_cancel()
+                elif key == 'w':
+                    bin_id = 'bin_' + raw_input('  bin id? ')
+                    hmi_demo.clear_fail_poses()
+                    hmi_demo.sweep_bin(bin_id)
+                    hmi_demo.go_to_named_pose(hmi_demo.former_robot_name, 'home')
+                elif key == 'd':
+                    bin_id = 'bin_' + raw_input('  bin id? ')
+                    hmi_demo.clear_fail_poses()
+                    hmi_demo.demo(bin_id, 1)
+                elif key == 'k':
+                    hmi_demo.run()
+                elif key == 'g':
+                    if hmi_demo.former_robot_name is not None:
+                        hmi_demo.grasp(hmi_demo.former_robot_name)
+                elif key == 'r':
+                    if hmi_demo.former_robot_name is not None:
+                        hmi_demo.release(hmi_demo.former_robot_name)
+            except Exception as e:
+                print(e.message)
---
+++ aist_routines/scripts/hmi_server.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+#
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2021, National Institute of Advanced Industrial Science and Technology (AIST)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of National Institute of Advanced Industrial
+#    Science and Technology (AIST) nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Toshio Ueshiba
+#
+import rospy
+from finger_pointing_msgs.msg import (RequestHelpAction,
+                                      RequestHelpGoal, RequestHelpResult,
+                                      request_help, pointing)
+from actionlib                import SimpleActionServer
+
+######################################################################
+#  class HMIServer                                                   #
+######################################################################
+class HMIServer(object):
+    def __init__(self):
+        super(HMIServer, self).__init__()
+
+        self._no_req     = request_help(request=request_help.NO_REQ)
+        self._curr_req   = self._no_req
+        self._prev_state = pointing.NO_RES
+        self._hmi_pub    = rospy.Publisher('request_help', request_help,
+                                           queue_size=10)
+        self._hmi_sub    = rospy.Subscriber('/pointing', pointing,
+                                            self._pointing_cb)
+        self._hmi_srv    = SimpleActionServer('~request_help',
+                                              RequestHelpAction,
+                                              auto_start=False)
+
+        self._hmi_srv.register_goal_callback(self._goal_cb)
+        self._hmi_srv.register_preempt_callback(self._preempt_cb)
+        self._hmi_srv.start()
+
+    def run(self):
+        rate = rospy.Rate(10)   # 10Hz
+        while not rospy.is_shutdown():
+            self._hmi_pub.publish(self._curr_req)
+            rate.sleep()
+
+    def _pointing_cb(self, pointing_msg):
+        if self._hmi_srv.is_active() and \
+           self._prev_state is pointing.NO_RES and \
+           pointing_msg.pointing_state is not pointing.NO_RES:
+            self._hmi_srv.set_succeeded(RequestHelpResult(pointing_msg))
+            self._curr_req = self._no_req
+
+        self._prev_state = pointing_msg.pointing_state
+
+    def _goal_cb(self):
+        self._curr_req = goal.request
+        rospy.loginfo('(hmi_server) ACCPETED new goal[robot_name=%s, item_id=%s. request=%d]',
+                      self._curr_req.robot_name, self._curr_req.item_id,
+                      self._curr_req.request)
+
+    def _preempt_cb(self):
+        self._request.set_preempted()
+        self._request = self._no_req
+        rospy.loginfo('(hmi_server) PREEMPTED current goal')
+
+#########################################################################
+#  entry point                                                          #
+#########################################################################
+if __name__ == '__main__':
+    try:
+        rospy.init_node('hmi_server')
+        hmi_srv = HMIServer()
+        hmi_srv.run()
+    except rospy.ROSInitException as e:
+        rospy.logerr(e)
+    except rospy.ROSInterruptException:
+        pass
