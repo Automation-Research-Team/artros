7b22f809e8da5bc7896dc143b2aa75b82a9c2043
Toshio UESHBIA <t.ueshiba@aist.go.jp>
2022-09-26 08:17:14 +0900

    Add pose_head_servo action server

--- aist_controllers/src/pose_head_servo.cpp
+++ aist_controllers/src/pose_head_servo.cpp
@@ -1,36 +1,39 @@
 /*
- *  \file	pose_tracking_servo.cpp
+ *  \file	pose_head_servo.cpp
  *  \brief	ROS tracker of aist_controllers::PoseHeadAction type
  */
 #include <std_msgs/Int8.h>
 #include <std_srvs/SetBool.h>
 #include <geometry_msgs/TransformStamped.h>
-
+#include <actionlib/server/simple_action_server.h>
 #include <moveit_servo/servo.h>
 #include <moveit_servo/pose_tracking.h>
 #include <moveit_servo/status_codes.h>
 #include <moveit_servo/make_shared_from_pool.h>
 #include <thread>
 
+#include <aist_controllers/PoseHeadAction.h>
+
 namespace aist_controllers
 {
-static const std::string LOGNAME = "pose_tracking_servo";
+static const std::string LOGNAME = "pose_head_servo";
 
 /************************************************************************
-*  class PoseTrackingServo						*
+*  class PoseHeadServo							*
 ************************************************************************/
-class PoseTrackingServo
+class PoseHeadServo
 {
   private:
     using planning_scene_monitor_t
 		= planning_scene_monitor::PlanningSceneMonitor;
     using planning_scene_monitor_p
 		= planning_scene_monitor::PlanningSceneMonitorPtr;
+    using server_t = actionlib::SimpleActionServer<PoseHeadAction>;
 
     class StatusMonitor
     {
       private:
-	using StatusCode	= moveit_servo::StatusCode;
+	using StatusCode = moveit_servo::StatusCode;
 
       public:
 	StatusMonitor(ros::NodeHandle& nh, const std::string& topic)
@@ -50,7 +53,7 @@ class PoseTrackingServo
 		_status = latest_status;
 
 		ROS_INFO_STREAM_NAMED(
-		    LOGNAME, "Servo status: "
+		    LOGNAME, "(PoseHeadServo) Servo status: "
 		    << moveit_servo::SERVO_STATUS_CODE_MAP.at(_status));
 	    }
 	}
@@ -61,8 +64,8 @@ class PoseTrackingServo
     };
 
   public:
-		PoseTrackingServo()					;
-		~PoseTrackingServo()					;
+		PoseHeadServo(const std::string& action_ns)		;
+		~PoseHeadServo()					;
 
     void	run()							;
 
@@ -70,8 +73,7 @@ class PoseTrackingServo
     static planning_scene_monitor_p
 		create_planning_scene_monitor(
 		    const std::string& robot_description)		;
-    bool	set_active_cb(std_srvs::SetBool::Request&  req,
-			      std_srvs::SetBool::Response& res)		;
+    void	execute_cb(const PoseHeadGoalConstPtr& goal)		;
 
   private:
     ros::NodeHandle		_nh;
@@ -79,26 +81,36 @@ class PoseTrackingServo
     moveit_servo::PoseTracking	_tracker;
     StatusMonitor		_status_monitor;
 
+    server_t			_tracker_srv;
+    const ros::Publisher	_target_pose_pub;
+
     Eigen::Vector3d		_lin_tol{0.01, 0.01, 0.01};
     double			_rot_tol = 0.1;
 
     std::thread			_move_to_pose_thread;
 };
 
-PoseTrackingServo::PoseTrackingServo()
+PoseHeadServo::PoseHeadServo(const std::string& action_ns)
     :_nh("~"),
      _planning_scene_monitor(create_planning_scene_monitor(
 				 "robot_description")),
      _tracker(_nh, _planning_scene_monitor),
      _status_monitor(_nh, "status"),
+     _tracker_srv(_nh, action_ns,
+		  boost::bind(&PoseHeadServo::execute_cb, this, _1),
+		  false),
+     _target_pose_pub(_nh.advertise<geometry_msgs::PoseStamped>("target_pose",
+								1, true)),
      _lin_tol{0.01, 0.01, 0.01},
      _rot_tol(0.1),
      _move_to_pose_thread()
 {
-    ROS_INFO_STREAM_NAMED(LOGNAME, "PoseTrackingServo initialized.");
+    _tracker_srv.start();
+
+    ROS_INFO_STREAM_NAMED(LOGNAME, "(PoseHeadServo) initialized");
 }
 
-PoseTrackingServo::~PoseTrackingServo()
+PoseHeadServo::~PoseHeadServo()
 {
     _tracker.stopMotion();
     if (_move_to_pose_thread.joinable())
@@ -106,7 +118,7 @@ PoseTrackingServo::~PoseTrackingServo()
 }
 
 void
-PoseTrackingServo::run()
+PoseHeadServo::run()
 {
     ros::AsyncSpinner	spinner(8);
     spinner.start();
@@ -115,7 +127,7 @@ PoseTrackingServo::run()
 }
 
 planning_scene_monitor::PlanningSceneMonitorPtr
-PoseTrackingServo::create_planning_scene_monitor(
+PoseHeadServo::create_planning_scene_monitor(
 			const std::string& robot_description)
 {
     using	namespace planning_scene_monitor;
@@ -125,7 +137,7 @@ PoseTrackingServo::create_planning_scene_monitor(
     if (!monitor->getPlanningScene())
     {
 	ROS_ERROR_STREAM_NAMED(
-	    LOGNAME, "Error in setting up the PlanningSceneMonitor.");
+	    LOGNAME, "(PoseHeadServo) failed to get PlanningSceneMonitor");
 	exit(EXIT_FAILURE);
     }
 
@@ -136,58 +148,41 @@ PoseTrackingServo::create_planning_scene_monitor(
 	false /* skip octomap monitor */);
     monitor->startStateMonitor();
 
-    ROS_INFO_STREAM_NAMED(LOGNAME, "PlanningSceneMonitor started.");
+    ROS_INFO_STREAM_NAMED(LOGNAME,
+			  "(PoseHeadServo) PlanningSceneMonitor started");
 
     return monitor;
 }
 
-bool
-PoseTrackingServo::set_active_cb(std_srvs::SetBool::Request&  req,
-				 std_srvs::SetBool::Response& res)
+void
+PoseHeadServo::execute_cb(const PoseHeadGoalConstPtr& goal)
 {
-    if (req.data)
-    {
-	if (!_move_to_pose_thread.joinable())
-	{
-	    _tracker.resetTargetPose();
-	    _move_to_pose_thread = std::thread([this]
-					       {
-						   this->_tracker.moveToPose(
-						       this->_lin_tol,
-						       this->_rot_tol, 0.1);
-					       });
+    ROS_INFO_STREAM_NAMED(LOGNAME, "(PoseHeadServo) goal ACCEPTED");
 
-	    res.success = true;
-	    res.message = "tracking servo activated";
-	    ROS_INFO_STREAM_NAMED(LOGNAME, res.message);
-	}
-	else
-	{
-	    res.success = true;
-	    res.message = "tracking servo already activated";
-	    ROS_WARN_STREAM_NAMED(LOGNAME, res.message);
-	}
-    }
-    else
-    {
-	if (_move_to_pose_thread.joinable())
-	{
-	    _move_to_pose_thread.join();
+    _tracker.resetTargetPose();
+    _move_to_pose_thread = std::thread([this]
+				       {
+					   this->_tracker.moveToPose(
+					       this->_lin_tol,
+					       this->_rot_tol, 0.1);
+				       });
 
-	    res.success = true;
-	    res.message = "tracking servo deactivated";
-	    ROS_INFO_STREAM_NAMED(LOGNAME, res.message);
-	}
-	else
-	{
-	    res.success = true;
-	    res.message = "tracking servo already deactivated";
-	    ROS_WARN_STREAM_NAMED(LOGNAME, res.message);
-	}
+    for (ros::Rate rate(_nh.param<double>("rate", 50.0));
+	 !_tracker_srv.isPreemptRequested() && ros::ok(); rate.sleep())
+    {
+	geometry_msgs::PoseStamped	target_pose = goal->target;
+	target_pose.header.stamp = ros::Time::now();
+	_target_pose_pub.publish(target_pose);
     }
 
-    return true;
+    _tracker.stopMotion();
+    if (_move_to_pose_thread.joinable())
+	_move_to_pose_thread.join();
+    _tracker_srv.setPreempted();
+
+    ROS_WARN_STREAM_NAMED(LOGNAME, "(PoseHeadServo) goal CANCELED");
 }
+
 
 }	// namespace aist_controllers
 
@@ -199,7 +194,7 @@ main(int argc, char* argv[])
 {
     ros::init(argc, argv, aist_controllers::LOGNAME);
 
-    aist_controllers::PoseTrackingServo	servo;
+    aist_controllers::PoseHeadServo	servo("pose_head");
     servo.run();
 
     return 0;
