dadd62cd62c96f44d5eeaf3742b96de09a76187b
Toshio UESHBIA <t.ueshiba@aist.go.jp>
2022-11-23 11:54:28 +0900

    WIP: Implement gravity compensation without usin TF

--- aist_ftsensor/src/aist_ftsensor_controller.cpp
+++ aist_ftsensor/src/aist_ftsensor_controller.cpp
@@ -43,22 +43,18 @@
 #include <realtime_tools/realtime_publisher.h>
 #include <pluginlib/class_list_macros.hpp>
 #include <geometry_msgs/WrenchStamped.h>
+#include <sensor_msgs/JointState.h>
 #include <std_srvs/Trigger.h>
+#include <ddynamic_reconfigure/ddynamic_reconfigure.h>
+#include <kdl_parser/kdl_parser.hpp>
+#include <kdl/chain.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
 #include <fstream>
 #include <yaml-cpp/yaml.h>
 #include <cstdlib>		// for std::getenv()
 #include <sys/stat.h>		// for mkdir()
-#include <Eigen/Dense>
 #include <aist_utility/eigen.h>
 #include <aist_utility/butterworth_lpf.h>
-#include <ddynamic_reconfigure/ddynamic_reconfigure.h>
-#include <kdl/chainfksolver.hpp>
-#include <kdl/chain.hpp>
-#include <kdl/chainjnttojacsolver.hpp>
-#include <kdl/chainfksolverpos_recursive.hpp>
-#include <kdl/frames_io.hpp>
-#include <kdl/kinfam_io.hpp>
-#include <kdl_parser/kdl_parser.hpp>
 
 namespace aist_ftsensor
 {
@@ -81,6 +77,12 @@ operator <<(std::ostream& out, const std::vector<T>& v)
     return out << std::endl;
 }
 
+inline Eigen::Vector3d
+fromKDL(const KDL::Vector& v)
+{
+    return {v(0), v(1), v(2)};
+}
+
 /************************************************************************
 *  class ForceTorqueSensorController					*
 ************************************************************************/
@@ -89,7 +91,8 @@ class ForceTorqueSensorController
 		 hardware_interface::ForceTorqueSensorInterface>
 {
   private:
-    using interface_t	= hardware_interface::ForceTorqueSensorInterface;
+    using interface_t	 = hardware_interface::ForceTorqueSensorInterface;
+    using joint_state_cp = sensor_msgs::JointStateConstPtr;
 
     class Sensor
     {
@@ -98,6 +101,8 @@ class ForceTorqueSensorController
 	using publisher_t	= realtime_tools::RealtimePublisher<
 					geometry_msgs::WrenchStamped>;
 	using publisher_p	= std::shared_ptr<publisher_t>;
+	using fksolver_p	= std::unique_ptr<KDL::ChainFkSolverPos>;
+	using controller_t	= ForceTorqueSensorController;
 	using vector_t		= Eigen::Vector3d;
 	using matrix_t		= Eigen::Matrix3d;
 	using quaternion_t	= Eigen::Quaterniond;
@@ -122,7 +127,7 @@ class ForceTorqueSensorController
       public:
 		Sensor(interface_t* hw, ros::NodeHandle& root_nh,
 		       const std::string& name, double pub_rate,
-		       const KDL::Tree& tree)				;
+		       const controller_t& controller)			;
 
 	void	starting(const ros::Time& time)				;
 	void	update(const ros::Time& time,
@@ -158,24 +163,26 @@ class ForceTorqueSensorController
 	const ros::ServiceServer	_take_sample;
 	const ros::ServiceServer	_compute_calibration;
 	const ros::ServiceServer	_clear_samples;
+	ddr_t				_ddr;
 
-	const KDL::Tree&		_tree;
+      // Filtering stuffs
+	ft_t				_ft;
+	filter_t			_filter;
+
+      // Forward kinematics stuffs
+	const controller_t&		_controller;
 	KDL::Chain			_chain;
-	
+	std::vector<std::string>	_joint_names;
+	KDL::JntArray			_joint_positions;
+	fksolver_p			_fksolver;
+
       // Variables retrieved from parameter server
-	std::string			_gravity_frame;
-	double				_rate;
+	bool				_compensate_gravity;
 	double				_mg;		// effector mass
 	quaternion_t			_q;		// rotation
 	vector_t			_r;		// mass center
 	vector_t			_f0;		// force offset
 	vector_t			_m0;		// torque offset
-
-      // DDynamicReconfigure stuffs: filtering and gravity compensation
-	ft_t				_ft;
-	filter_t			_filter;
-	bool				_compensate_gravity;
-	ddr_t				_ddr;
 
       // Calibration stuffs
 	bool				_do_sample;
@@ -204,22 +211,35 @@ class ForceTorqueSensorController
 			       const ros::Duration& period)		;
     virtual void	stopping(const ros::Time& time)			;
 
+    const KDL::Tree&	get_tree()				const	;
+    void		get_jnt_pos(const std::vector<std::string>& jnt_name,
+				    KDL::JntArray& jnt_pos)	const	;
+
   private:
+    void	joint_state_cb(const joint_state_cp& joint_state)	;
     bool	save_calibration_cb(std_srvs::Trigger::Request&  req,
 				    std_srvs::Trigger::Response& res)	;
 
   private:
-    std::vector<sensor_p>	_sensors;
-    std::string			_calib_file;
-    ros::ServiceServer		_save_calibration;
-    KDL::Tree			_tree;
+  // JointState stuffs
+    KDL::Tree				_tree;
+    ros::Subscriber			_joint_state_sub;
+    std::map<std::string, double>	_joint_positions;
+    mutable std::mutex			_joint_state_mtx;
+
+  // Calibration stuffs
+    std::string				_calib_file;
+    ros::ServiceServer			_save_calibration;
+
+    std::vector<sensor_p>		_sensors;
 };
 
 ForceTorqueSensorController::ForceTorqueSensorController()
-    :_sensors(), _calib_file(), _save_calibration(), _urdf(), _tree()
+    :_tree(), _joint_state_sub(), _joint_positions(), _joint_state_mtx(),
+     _calib_file(), _save_calibration(), _sensors(0)
 {
 }
-    
+
 bool
 ForceTorqueSensorController::init(interface_t* hw,
 				  ros::NodeHandle& root_nh,
@@ -236,7 +256,7 @@ ForceTorqueSensorController::init(interface_t* hw,
 			 << param_name << "] not found");
 	return false;
     }
-	
+
   // Construct KDL tree from robot_description parameter.
     if (!kdl_parser::treeFromString(robot_desc_string, _tree))
     {
@@ -244,22 +264,13 @@ ForceTorqueSensorController::init(interface_t* hw,
 			 << "Failed to construct kdl tree");
 	return false;
     }
-    
-  // Get publishing period.
-    double	pub_rate;
-    if (!controller_nh.getParam("publish_rate", pub_rate))
-    {
-	ROS_ERROR_STREAM("(aist_ftsensor_controller) Parameter 'publish_rate' not set");
-	return false;
-    }
 
-  // Setup sensors.
-    for (const auto& name : hw->getNames())
-	_sensors.push_back(sensor_p(new Sensor(hw, root_nh, name,
-					       pub_rate, _tree)));
+  // Create subscriber for joint_states
+    _joint_state_sub = root_nh.subscribe("/joint_states", 1,
+					 &ForceTorqueSensorController
+					 ::joint_state_cb, this);
 
-  // Get namespace for saving calibration.
-  // Read calibration file name from parameter server.
+  // Get calibration file name from parameter server.
     _calib_file = root_nh.param<std::string>("calib_file",
 					     std::string(getenv("HOME")) +
 					     "/.ros/aist_ftsensor" +
@@ -268,6 +279,21 @@ ForceTorqueSensorController::init(interface_t* hw,
 			    "save_calibration",
 			    &ForceTorqueSensorController::save_calibration_cb,
 			    this);
+
+  // Get publishing period.
+    const auto	pub_rate = controller_nh.param<double>("publish_rate", 0.0);
+    if (pub_rate <= 0.0)
+    {
+	ROS_ERROR_STREAM("(aist_ftsensor_controller) "
+			 << "Value of parameter 'publish_rate' is "
+			 << pub_rate << ", but must be positive.");
+	return false;
+    }
+
+  // Setup sensors.
+    for (const auto& name : hw->getNames())
+	_sensors.push_back(sensor_p(new Sensor(hw, root_nh, name,
+					       pub_rate, *this)));
 
     return true;
 }
@@ -292,6 +318,31 @@ ForceTorqueSensorController::stopping(const ros::Time& time)
 {
     for (const auto& sensor : _sensors)
 	sensor->stopping(time);
+}
+
+const KDL::Tree&
+ForceTorqueSensorController::get_tree() const
+{
+    return _tree;
+}
+
+void
+ForceTorqueSensorController::get_jnt_pos(
+    const std::vector<std::string>& jnt_name, KDL::JntArray& jnt_pos) const
+{
+    std::lock_guard<std::mutex>	lock(_joint_state_mtx);
+
+    for (size_t i = 0; i < jnt_name.size(); ++i)
+	jnt_pos(i) = _joint_positions.at(jnt_name[i]);
+}
+
+void
+ForceTorqueSensorController::joint_state_cb(const joint_state_cp& joint_state)
+{
+    std::lock_guard<std::mutex>	lock(_joint_state_mtx);
+
+    for (size_t i = 0; i < joint_state->name.size(); ++i)
+	_joint_positions[joint_state->name[i]] = joint_state->position[i];
 }
 
 bool
@@ -335,9 +386,11 @@ ForceTorqueSensorController::save_calibration_cb(
 /************************************************************************
 *  class ForceTorqueSensorController::Sensor				*
 ************************************************************************/
-ForceTorqueSensorController::Sensor::Sensor(
-    interface_t* hw, ros::NodeHandle& root_nh, const std::string& name,
-    double pub_rate, const KDL::Tree& tree)
+ForceTorqueSensorController::Sensor::Sensor(interface_t* hw,
+					    ros::NodeHandle& root_nh,
+					    const std::string& name,
+					    double pub_rate,
+					    const controller_t& controller)
     :_hw_handle(hw->getHandle(name)),
      _pub_org(new publisher_t(root_nh, name + "_org", 4)),
      _pub(new publisher_t(root_nh, name, 4)),
@@ -351,17 +404,20 @@ ForceTorqueSensorController::Sensor::Sensor(
 					       this)),
      _clear_samples(_nh.advertiseService("clear_samples",
 					 &Sensor::clear_samples_cb, this)),
-     _tree(tree),
-     _gravity_frame(_nh.param<std::string>("gravity_frame", "world")),
+     _ddr(_nh),
+     _ft(ft_t::Zero()),
+     _filter(2, 7.0*_pub_interval.toSec()),
+     _controller(controller),
+     _chain(),
+     _joint_names(),
+     _joint_positions(),
+     _fksolver(),
+     _compensate_gravity(_nh.param<bool>("compensate_gravity", false)),
      _mg(G*_nh.param<double>("effector_mass", 0.0)),
      _q(quaternion_param("rotation")),
      _r(vector_param("mass_center")),
      _f0(vector_param("force_offset")),
      _m0(vector_param("torque_offset")),
-     _ft(ft_t::Zero()),
-     _filter(2, 7.0*_pub_interval.toSec()),
-     _compensate_gravity(_nh.param<bool>("compensate_gravity", false)),
-     _ddr(_nh),
      _do_sample(false),
      _nsamples(0),
      _k_sum(vector_t::Zero()),
@@ -373,11 +429,26 @@ ForceTorqueSensorController::Sensor::Sensor(
      _mm_sum(matrix_t::Zero()),
      _fout()
 {
-  // Get chain from _base_link to pointing_frame.
-    if (!_tree.getChain(_gravity_frame, _hw_handle.getFrameId(), _chain))
+  // Get chain from gravity frame to sensor frame.
+    const auto	gravity_frame = _nh.param<std::string>("gravity_frame",
+						       "world");
+    if (!_controller.get_tree().getChain(gravity_frame,
+					 _hw_handle.getFrameId(), _chain))
 	throw std::runtime_error("Couldn't create chain from "
-				 + _gravity_frame + " to "
+				 + gravity_frame + " to "
 				 + _hw_handle.getFrameId());
+
+  // Get names of joints contained in the chain.
+    for (size_t i = 0; i < _chain.getNrOfSegments(); ++i)
+    {
+	const auto&	joint = _chain.getSegment(i).getJoint();
+	if (joint.getType() != KDL::Joint::None)
+	    _joint_names.push_back(joint.getName());
+    }
+    _joint_positions.resize(_joint_names.size());
+
+  // Create FK solver for the chain.
+    _fksolver.reset(new KDL::ChainFkSolverPos_recursive(_chain));
 
   // Setup dynamic reconfigure server
     _ddr.registerVariable<int>(
@@ -406,94 +477,77 @@ void
 ForceTorqueSensorController::Sensor::update(const ros::Time& time,
 					    const ros::Duration& period)
 {
-    if (_pub_interval > ros::Duration(0.0) &&
-	_last_pub_time + _pub_interval < time)
+    if (time - _last_pub_time < _pub_interval)
+	return;
+
+  // Get current force-torque values.
+    _ft(0) = _hw_handle.getForce()[0];
+    _ft(1) = _hw_handle.getForce()[1];
+    _ft(2) = _hw_handle.getForce()[2];
+    _ft(3) = _hw_handle.getTorque()[0];
+    _ft(4) = _hw_handle.getTorque()[1];
+    _ft(5) = _hw_handle.getTorque()[2];
+
+  // Publish unfiltered force-torque signal.
+    if (_pub_org->trylock())
     {
-      // Get current force-torque values.
-	_ft(0) = _hw_handle.getForce()[0];
-	_ft(1) = _hw_handle.getForce()[1];
-	_ft(2) = _hw_handle.getForce()[2];
-	_ft(3) = _hw_handle.getTorque()[0];
-	_ft(4) = _hw_handle.getTorque()[1];
-	_ft(5) = _hw_handle.getTorque()[2];
+	_pub_org->msg_.header.stamp    = time;
+	_pub_org->msg_.header.frame_id = _hw_handle.getFrameId();
+	_pub_org->msg_.wrench.force.x  = _ft(0);
+	_pub_org->msg_.wrench.force.y  = _ft(1);
+	_pub_org->msg_.wrench.force.z  = _ft(2);
+	_pub_org->msg_.wrench.torque.x = _ft(3);
+	_pub_org->msg_.wrench.torque.y = _ft(4);
+	_pub_org->msg_.wrench.torque.z = _ft(5);
 
-	const auto& frame_id = _hw_handle.getFrameId();
+	_pub_org->unlockAndPublish();
+    }
 
-      // Publish unfiltered force-torque signal.
-	if (_pub_org->trylock())
+  // Apply low-pass filter to input force-torque signal.
+    auto	ft = _filter.filter(_ft);
+
+    if (_compensate_gravity)
+    {
+      // Lookup current positions of joints contained in the chain.
+	_controller.get_jnt_pos(_joint_names, _joint_positions);
+
+      // Get transform from sensor frame to gravity frame
+      // for current joint positions.
+	KDL::Frame	Tgs;
+	_fksolver->JntToCart(_joint_positions, Tgs);
+
+      // Get gravity direction w.r.t. sensor frame.
+	const vector_t	k = fromKDL(Tgs.M.Inverse(KDL::Vector(0, 0, -1)));
+
+      // Compute filtered force and torque.
+	const vector_t	f = ft.head<3>();
+	const vector_t	m = ft.tail<3>();
+
+	if (_do_sample)
 	{
-	    _pub_org->msg_.header.stamp    = time;
-	    _pub_org->msg_.header.frame_id = frame_id;
-	    _pub_org->msg_.wrench.force.x  = _ft[0];
-	    _pub_org->msg_.wrench.force.y  = _ft[1];
-	    _pub_org->msg_.wrench.force.z  = _ft[2];
-	    _pub_org->msg_.wrench.torque.x = _ft[3];
-	    _pub_org->msg_.wrench.torque.y = _ft[4];
-	    _pub_org->msg_.wrench.torque.z = _ft[5];
-
-	    _pub_org->unlockAndPublish();
+	    take_sample(k, f, m);
+	    _do_sample = false;
 	}
 
-      // Apply low-pass filter to input force-torque signal.
-	const auto	ft = _filter.filter(_ft);
+      // Compensate force/torque offsets and gravity.
+	ft.head<3>() = (_q.inverse()*(f - _f0) - _mg*k).eval();
+	ft.tail<3>() = (_q.inverse()*(m - _m0) - _r.cross(_mg*k)).eval();
+    }
 
-	if (_pub->trylock())
-	{
-	    vector_t	force, torque;
-	    
-	    if (_compensate_gravity)
-	    {
-	      // Compute gravty direction w.r.t. sensor frame, i.e. frame_id.
-		vector_t	k;
-		try
-		{
-		    tf2::doTransform(vector_t(0, 0, -1), k,
-				     _tf2_buffer.lookupTransform(
-					 frame_id, _gravity_frame,
-					 time, ros::Duration(0.1)));
-		}
-		catch (const std::exception& err)
-		{
-		    _pub->unlock();
-		    ROS_ERROR_STREAM("(aist_ftsensor) " << err.what());
-		    return;
-		}
+  // Publish filtered (and optionally gravity compensated) force-torque signal.
+    if (_pub->trylock())
+    {
+	_pub->msg_.header.stamp	   = time;
+	_pub->msg_.header.frame_id = _hw_handle.getFrameId();
+	_pub->msg_.wrench.force.x  = ft(0);
+	_pub->msg_.wrench.force.y  = ft(1);
+	_pub->msg_.wrench.force.z  = ft(2);
+	_pub->msg_.wrench.torque.x = ft(3);
+	_pub->msg_.wrench.torque.y = ft(4);
+	_pub->msg_.wrench.torque.z = ft(5);
 
-	      // Compute filtered force and torque.
-		const vector_t	f = ft.head<3>();
-		const vector_t	m = ft.tail<3>();
-
-		if (_do_sample)
-		{
-		    take_sample(k, f, m);
-		    _do_sample = false;
-		}
-
-	      // Compensate force/torque offsets and gravity.
-		force  = _q.inverse()*(f - _f0) - _mg*k;
-		torque = _q.inverse()*(m - _m0) - _r.cross(_mg*k);
-	    }
-	    else
-	    {
-		force  = ft.head<3>();
-		torque = ft.tail<3>();
-	    }
-	    
-	  // We're actually publishing, so increment time.
-	    _last_pub_time = _last_pub_time + _pub_interval;
-
-	  // Populate message.
-	    _pub->msg_.header.stamp    = time;
-	    _pub->msg_.header.frame_id = frame_id;
-	    _pub->msg_.wrench.force.x  = force(0);
-	    _pub->msg_.wrench.force.y  = force(1);
-	    _pub->msg_.wrench.force.z  = force(2);
-	    _pub->msg_.wrench.torque.x = torque(0);
-	    _pub->msg_.wrench.torque.y = torque(1);
-	    _pub->msg_.wrench.torque.z = torque(2);
-
-	    _pub->unlockAndPublish();
-	}
+	_pub->unlockAndPublish();
+	_last_pub_time = time;
     }
 }
 
