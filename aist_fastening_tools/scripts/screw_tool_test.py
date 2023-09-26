b42f99d26ee963f8a21307451ac9f51066154133
Toshio UESHBIA <t.ueshiba@aist.go.jp>
2023-09-26 09:46:09 +0900

    Remove unused files and improve test programs in aist_fastening_tools

--- aist_fastening_tools/scripts/screw_tool_test.py
+++ aist_fastening_tools/scripts/screw_tool_test.py
@@ -1,42 +1,67 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
-import sys
-import rospy
+import sys, rospy
+import numpy as np
 from actionlib                import SimpleActionClient
 from aist_fastening_tools.msg import (ScrewToolCommandAction,
                                       ScrewToolCommandGoal)
+from aist_utility.compat      import *
 
 
-class ScrewToolController:
-    def fasten(self, name):
-        client = SimpleActionClient('screw_tool_controller/command',
-                                    ScrewToolCommandAction)
-        client.wait_for_server()
+class ScrewToolTest(object):
+    def __init__(self):
+        self._client = SimpleActionClient('screw_tool_controller/command',
+                                          ScrewToolCommandAction)
+        self._client.wait_for_server()
 
+    def run(self):
+        tool_name = 'screw_tool_m3'
+        speed     = 100
+
+        while not rospy.is_shutdown():
+            key = raw_input('[%s:%d]> ' % (tool_name, speed))
+
+            print('====')
+            print('  q: quit this program')
+            print('  t: tighten the screw')
+            print('  l: loosen the screw')
+            print('  s: set tool speed')
+            print('  3: switch the tool to m3')
+            print('  4: switch the tool to m4')
+
+            if key == 'q':
+                break
+            elif key == 't':
+                self._send_command(tool_name, 'tighten')
+            elif key == 'l':
+                self._send_command(tool_name, 'loosen')
+            elif key == 's':
+                speed = np.clip(int(raw_input('  speed? ')), 0, 1023)
+            elif key == '3':
+                tool_name = 'screw_tool_m3'
+            elif key == '4':
+                tool_name = 'screw_tool_m4'
+            else:
+                print('Unknown command[%s]' % key)
+
+    def _send_command(self, tool_name, speed, direction):
         goal = ScrewToolCommandGoal()
-        goal.fastening_tool_name = name
-        goal.speed = 100
-        goal.direction = "tighten"
+        goal.fastening_tool_name = tool_name
+        goal.speed               = speed
+        goal.direction           = direction
+        self._client.send_goal_and_wait(goal,
+                                        rospy.Duration(10), rospy.Duration(10))
+        self._client.wait_for_result()
 
-        client.send_goal_and_wait(goal, rospy.Duration(10), rospy.Duration(10))
-        client.wait_for_result()
-
-        return client.get_result()
+        return self._client.get_result()
 
 
 if __name__ == '__main__':
     try:
         rospy.init_node('screw_tool_test')
 
-        controller = ScrewToolController()
-
-        name_list = ["screw_tool_m4", "screw_tool_m3"]
-
-        for name in name_list:
-            if not controller.fasten(name):
-                break
-
-        print('')
+        test = ScrewToolTest()
+        test.run()
 
     except rospy.ROSInterruptException:
-        print("program interrupted before completion", file=sys.stderr)
+        print('program interrupted before completion', file=sys.stderr)
--- aist_fastening_tools/scripts/suction_tool_test.py
+++ aist_fastening_tools/scripts/suction_tool_test.py
@@ -1,46 +1,75 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
-import sys
-import rospy
+import sys, rospy
 from actionlib                import SimpleActionClient
 from aist_fastening_tools.msg import (SuctionToolCommandAction,
                                       SuctionToolCommandGoal)
+from aist_utility.compat      import *
 
-class SuctionToolControllerClient:
-    def suction(self, name, turn_suction_on, eject_screw):
-        client = SimpleActionClient('suction_tool_controller/command',
-                                    SuctionToolCommandAction)
-        client.wait_for_server()
+
+class SuctionToolTest(object):
+    def __init__(self):
+        self._client = SimpleActionClient('screw_tool_controller/command',
+                                          SuctionToolCommandAction)
+        self._client.wait_for_server()
+
+    def run(self):
+        tool_name = 'screw_tool_m3'
+        suction   = False
+        eject     = False
+
+        while not rospy.is_shutdown():
+            key = raw_input('%s:(suction=%d, eject=%d)> '
+                            % (tool_name, suction, eject))
+
+            print('====')
+            print('  q: quit this program')
+            print('  s: toggle suction')
+            print('  e: toggle eject')
+            print('  3: switch the tool to m3')
+            print('  4: switch the tool to m4')
+            print('  6: switch the tool to m6')
+            print('  S: switch the tool to suction_tool')
+
+            if key == 'q':
+                break
+            elif key == 's':
+                suction = not suction
+                success = self._send_command(tool_name, suction, eject)
+                print('  %s' % 'succeded' if success else 'failed')
+            elif key == 'e':
+                eject   = not eject
+                success = self._send_command(tool_name, suction, eject)
+                print('  %s' % 'succeded' if success else 'failed')
+            elif key == '3':
+                tool_name = 'screw_tool_m3'
+            elif key == '4':
+                tool_name = 'screw_tool_m4'
+            elif key == '6':
+                tool_name = 'screw_tool_m6'
+            elif key == 'S':
+                tool_name = 'suction_tool'
+            else:
+                print('Unknown command[%s]' % key)
+
+    def _send_command(self, tool_name, suction, eject):
         goal = SuctionToolCommandGoal()
+        goal.fastening_tool_name = tool_name
+        goal.turn_suction_on     = suction
+        goal.eject               = eject
+        self._client.send_goal_and_wait(goal,
+                                        rospy.Duration(30), rospy.Duration(10))
+        self._client.wait_for_result()
 
-        goal.fastening_tool_name = name
-        goal.turn_suction_on     = turn_suction_on
-        goal.eject_screw         = eject_screw
+        return self._client.get_result().success
 
-        client.send_goal_and_wait(goal,rospy.Duration(30), rospy.Duration(10))
-        client.wait_for_result()
-
-        return client.get_result()
 
 if __name__ == '__main__':
     try:
         rospy.init_node('suction_tool_test')
 
-        controller = SuctionToolControllerClient()
-
-        name_list = [
-            "screw_tool_m4"
-        ]
-
-        for name in name_list:
-            res = controller.suction(name, True, False)
-            rospy.sleep(10)
-            res = controller.suction(name, False, False)
-
-            if not res.success:
-                rospy.logerr("Can not pick screw")
-
-            rospy.sleep(1)
+        test = SuctionToolTest()
+        test.run()
 
     except rospy.ROSInterruptException:
-        print("program interrupted before completion", file=sys.stderr)
+        print('program interrupted before completion', file=sys.stderr)
