c189bf0cb17a8ecea3cbeff9ebcbcab0c219ff5e
Toshio UESHIBA <t.ueshiba@aist.go.jp>
2023-09-28 17:42:06 +0900

    Confirmed that screw_tool_controller.py works

--- aist_fastening_tools/scripts/screw_tool_controller.py
+++ aist_fastening_tools/scripts/screw_tool_controller.py
@@ -3,31 +3,36 @@
 import threading, rospy
 import numpy as np
 
-from actionlib                      import ActionServer
-from actionlib_msgs.msg             import GoalStatus
-from aist_fastening_tools.msg       import (ScrewToolCommandAction,
-                                            ScrewToolCommandResult,
-                                            ScrewToolCommandFeedback)
-from aist_utility.thread_with_trace import ThreadTrace
-from collections                    import deque
+from actionlib                    import ActionServer
+from actionlib_msgs.msg           import GoalStatus
+from aist_fastening_tools.msg     import (ScrewToolCommandAction,
+                                          ScrewToolCommandResult,
+                                          ScrewToolCommandFeedback)
+from collections                  import deque
+from dynamixel_workbench_msgs.msg import DynamixelStateList
+from dynamixel_workbench_msgs.srv import DynamixelCommand
 
-from dynamixel_workbench_msgs.msg   import DynamixelState, DynamixelStateList
-from dynamixel_workbench_msgs.srv   import DynamixelCommand
-
-
+#########################################################################
+#  class ScrewToolController                                            #
+#########################################################################
 class ScrewToolController(object):
     def __init__(self):
         super(ScrewToolController, self).__init__()
 
         self._name = rospy.get_name()
-        self._lock = threading.RLock()  # Protect self._active_goal_handles
+        self._lock = threading.RLock()
 
-        # initialize motor id table
+        # Set parameters
+        self._history_length    = rospy.get_param('~history_length',     2)
+        self._speed_threshold   = rospy.get_param('~speed_threshold',   18)
+        self._current_threshold = rospy.get_param('~current_threshold', 10)
+
+        # Initialize motor id table
         self._motor_ids    = {}
         self._goal_handles = {}
         for tool_name, tool_props in rospy.get_param('~screw_tools').items():
-            self._motor_ids[tool_name]          = tool_props['ID']
-            self._active_goal_handles[tool_name] = None
+            self._motor_ids[tool_name]    = tool_props['ID']
+            self._goal_handles[tool_name] = None
             rospy.loginfo("(%s) Loaded %s with id=%d",
                           self._name, tool_name, self._motor_ids[tool_name])
 
@@ -40,207 +45,176 @@ class ScrewToolController(object):
                                                       self._state_list_cb)
 
         # Create a service client for sending command to Dynamixel driver
+        rospy.wait_for_service(driver_ns + '/dynamixel_command')
         self._dynamixel_command = rospy.ServiceProxy(driver_ns
                                                      + '/dynamixel_command',
                                                      DynamixelCommand)
-        #rospy.wait_for_service(driver_ns + '/dynamixel_command')
 
         self._server = ActionServer('~command', ScrewToolCommandAction,
                                     self._goal_cb, auto_start=False)
         self._server.start()
-
         rospy.loginfo('(%s) controller started', self._name)
 
     def _state_list_cb(self, state_list):
-        """
-        This function is executed when a message is received by
-        _dynamixel_state_sub. It stores the status of the motors connected
-        to the controller.
-        """
         self._dynamixel_states = state_list.dynamixel_state
 
     def _goal_cb(self, goal_handle):
         goal = goal_handle.get_goal()
 
-        if goal.tool_name not in self._active_goal_handles:
+        # Check if given tool name is valid.
+        if goal.tool_name not in self._goal_handles:
             goal_handle.set_rejected()
-            rospy.logerr('(%s) goal REJECTED: tool named %s not found',
+            rospy.logerr('(%s) goal REJECTED: tool[%s] not found',
                          self._name, goal.tool_name)
             return
 
-        if self._active_goal_handles[goal.tool_name] is not None:
-            goal_handle.set_rejected()
-            rospy.logerr('(%s) goal REJECTED: tool named %s is busy',
-                         self._name, goal.tool_name)
-            return
+        active_goal_handle = self._goal_handles[goal.tool_name]
+        if active_goal_handle is not None:
+            active_goal_handle.set_canceled()
+            with self._lock:
+                self._goal_handles[goal.tool_name] = None
+            rospy.logwarn('(%s) current active goal[%s] CANCELED because another goal for tool[%s] is received',
+                          self._name,
+                          active_goal_handle.get_goal_id().id, goal.tool_name)
 
         goal_handle.set_accepted()
         rospy.loginfo('(%s) new goal ACCEPTED for %s',
                       self._name, goal.tool_name)
 
-        goal_thread = ThreadTrace(target=self.execute_control,
-                                  args=[goal_handle])
+        goal_thread = threading.Thread(target=self.execute_control,
+                                       args=(goal_handle,
+                                             goal_handle.get_goal().retighten))
         goal_thread.daemon = True
         goal_thread.start()
-        return
 
-    def execute_control(self, goal_handle, double_check_after_tighten=True):
+    def execute_control(self, goal_handle, retighten):
         """
         Execute the tighten/loosen action.
         If double_check_after_tighten is True, after fastening has finished
         successfully, the screw is loosened once and then fastened again.
         """
-        with self._lock:
-            self._active_goal_handles[goal.tool_name] = goal_handle
-
         goal = goal_handle.get_goal()
+
+        with self._lock:
+            self._goal_handles[goal.tool_name] = goal_handle
+
         if not goal.speed:
             goal.speed = 1023  # Maximum speed
-        if not goal.timeout:
-            goal.timeout = rospy.Duration(10.0 if goal.tighten else 2.0)
 
         motor_id = self._motor_ids[goal.tool_name]
-        result   = ScrewToolCommandResult(success=True, stalled=False)
-        feedback = ScrewToolCommandFeedback(speed=goal.speed)
 
-        if goal.skip_final_loosen_and_retighten:
-            double_check_after_tighten = False
-
-        # For tightening the maximum speed is 1023. For loosen it is 2047.
+        # For tightening, i.e. CCW rotation, the maximum speed is 1023.
+        # For loosen, i.e. CW rotation, it is 2047.
         # At 1024 the motor is stopped.
-        if goal.tighten:  # CCW rotation
-            target_speed = np.clip(goal.speed, 0, 1023)
-        else:             # CW rotation
-            target_speed = np.clip(goal.speed, 0, 1023) + 1024
+        target_speed = np.clip(goal.speed, 0, 1023) \
+                     + (0 if goal.tighten else 1024)
 
-        if not self._set_value(motor_id, 'Moving_Speed', target_speed):
-            self._set_value(motor_id, 'Moving_Speed',  1024)  # Stop
-            self._set_value(motor_id, 'Torque_Enable', 0)
-            result.success = False
-            goal_handle.set_aborted(result)
-            rospy.logerr('(%s) goal ABORTED: failed to set speed[%d] for %s',
-                         self._name, target_speed, goal.tool_name)
-            return False
+        # Set target speed.
+        self._set_value(motor_id, 'Moving_Speed', target_speed)
 
         # Initialize to start the loop
-        speed_readings = deque([goal.speed, goal.speed], maxlen=2)
-        start_time     = rospy.Time.now()
-        rate           = rospy.Rate(20)
+        state_hitory = deque(maxlen=self._history_length)
+        rate         = rospy.Rate(20)
+
         # Wait for motor to start up (and avoid reading incorrect speed values)
         rospy.sleep(1)
         while not rospy.is_shutdown():
-
-            # Check if the goal is canceled.
+            # Check if the goal is canceled from client.
             if goal_handle.get_goal_status().status in (GoalStatus.PREEMPTING,
                                                         GoalStatus.RECALLING):
                 goal_handle.set_canceled()
-                rospy.logwarn('(%s) goal CANCELED: Motor pre-empted. Breaking out of loop to make room for next thread. Timeout: %s, Time elapsed: %s',
-                              self._name,
-                              goal.timeout.to_sec(), time_elapsed.to_sec())
+                rospy.logwarn('(%s) goal CANCELED by client', self._name)
                 break
 
-            # Check if timeout has expired.
-            time_elapsed = rospy.Time.now() - start_time
-            if time_elapsed > goal.timeout:
-                rospy.logwarn("(%s) motor stopped due to timeout[%f sec]. Time elapsed: %f sec",
-                              self._name,
-                              goal.timeout.to_sec(), time_elapsed.to_sec())
-                if goal.tighten:  # If motor does not stall before timeout, tightening was not successful
-                    result.success = False
-                    goal_handle.set_aborted(result)
-                    rospy.logerr('(%s) goal ABORTED: screw not fastened',
-                                 self._name)
-                else:
-                    result.success = True
-                    goal_handle.set_succeeded(result)
-                    rospy.loginfo('(%s) goal SUCCEDED: screw loosened',
-                                  self._name)
-                break
+            # Check if the goal is canceled within the server.
+            if goal_handle.get_goal_status().status == GoalStatus.PREEMPTED:
+                # Return without setting zero speed, because another goal
+                # has already been activated and target speed has been set.
+                return
 
-            # Check if the motor is stalled.
-            if feedback.speed == 0 and goal.tighten:
-                if double_check_after_tighten:
-                    rospy.loginfo('(%s) Motor has stalled. Loosening for 1 second and then trying to tighten again to confirm success.',
-                                  self._name)
-                    # Turn into loosening direction for 1sec.
-                    self._set_value(motor_id, 'Moving_Speed', 2047)
-                    rospy.sleep(1.0)
-                    self._set_value(motor_id, 'Moving_Speed', 1024)  # CW Stop
-
-                    return self.execute_control(goal_handle, False)
-
-                result.success = True
-                result.stalled = True
-                goal_handle.set_succeeded(result)
-                rospy.loginfo('(%s) goal SUCCEEDED: motor stalled and the screw  tightened',
-                              self._name)
-                break
-
-            # Read motor speed.
+            # Read the state of Dynamixel and keep it in the queue.
             state = self._get_dynamixel_state(motor_id)
             if state is None:
-                result.success = False
-                goal_handle.set_aborted(result)
-                rospy.logerr("(%s) goal ABORTED: error in motor readout",
-                              self._name)
+                goal_handle.set_aborted()
+                rospy.logerr("(%s) goal ABORTED: error in motor state readout",
+                             self._name)
                 break
-            speed = state.current_velocity
-            speed_readings.append(speed)
+            state_hitory.append(state)
 
-            # If both readings are below an arbitrary threshold,
-            # we assume the motor has stalled
-            stall_threshold = 18
-            if all(speed <= stall_threshold for speed in speed_readings):
-                feedback.speed = 0
+            # Publish feedback.
+            goal_handle.publish_feedback(ScrewToolCommandFeedback(
+                state.present_velocity,
+                state.present_current))
+
+            if goal.tighten:
+                # Check if the motor is stalled.
+                if len(state_hitory) == state_hitory.maxlen and \
+                   all(state.present_velocity <= self._speed_threshold
+                       for state in state_hitory):
+                    if retighten:
+                        # Turn into loosening direction for 1sec.
+                        self._set_value(motor_id, 'Moving_Speed', 2047)
+                        rospy.sleep(1.0)
+                        self._set_value(motor_id, 'Moving_Speed', 0)  # Stop
+
+                        self.execute_control(goal_handle, False)
+                        return
+                    else:
+                        goal_handle.set_succeeded(ScrewToolCommandResult(True))
+                        rospy.loginfo('(%s) goal SUCCEEDED: screw tightened',
+                                      self._name)
+                        break
             else:
-                feedback.speed = max(speed_readings)
+                # Check if the motor is free.
+                if len(state_hitory) == state_hitory.maxlen and \
+                   all(state.present_current <= self._current_threshold
+                       for state in state_hitory):
+                    goal_handle.set_succeeded(ScrewToolCommandResult(False))
+                    rospy.loginfo('(%s) goal SUCCEEDED: screw released',
+                                  self._name)
+                    break
 
-            goal_handle.publish_feedback(feedback)
             rate.sleep()
 
         # Stop rotation and disable torque.
-        self._set_value(motor_id, 'Moving_Speed',  1024)
+        self._set_value(motor_id, 'Moving_Speed',  0)
         self._set_value(motor_id, 'Torque_Enable', 0)
 
-        return result.success
+        with self._lock:
+            self._goal_handles[goal.tool_name] = None
 
     def _set_value(self, id, addr_name, value):
         try:
             res = self._dynamixel_command('', id, addr_name, value)
         except rospy.ServiceException as err:
-            rospy.logerr('(%s) failed to set value[%i] to %s: %s',
+            rospy.logerr('(%s) failed to set value[%d] to %s: %s',
                          self._name, value, addr_name, err)
             return False
 
         if res.comm_result:
-            rospy.loginfo("(%s) succesfully set value[%i] to %s",
+            rospy.loginfo("(%s) succesfully set value[%d] to %s",
                           self._name, value, addr_name)
         else:
-            rospy.logerr('(%s) communication error when setting value[%i] to %s',
+            rospy.logerr('(%s) communication error when setting value[%d] to %s',
                          self._name, value, addr_name)
         return res.comm_result
 
-    def _get_present_speed(self, motor_id):
-        state = self._get_dynamixel_state(motor_id)
-        return state.present_velocity if state is not None else None
-
-    def _get_present_current(self, motor_id):
-        state = self._get_dynamixel_state(motor_id)
-        return state.present_current if state is not None else None
-
-    def _get_dynamixel_state(self. motor_id):
+    def _get_dynamixel_state(self, motor_id):
         if not self._dynamixel_states:
             return None
 
         states = [state for state in self._dynamixel_states
                   if state.id == motor_id]
         if not states:
-            rospy.logerr('(%s) dynamixel state with ID=%i not found in state list',
+            rospy.logerr('(%s) dynamixel state with ID=%d not found in state list',
                          self._name, motor_id)
             return None
         return states[0]
 
 
+#########################################################################
+#  Entry point                                                          #
+#########################################################################
 if __name__ == '__main__':
     rospy.init_node('screw_tool_controller')
     server = ScrewToolController()
--- aist_fastening_tools/scripts/screw_tool_test.py
+++ aist_fastening_tools/scripts/screw_tool_test.py
@@ -3,6 +3,7 @@
 import sys, rospy
 import numpy as np
 from actionlib                import SimpleActionClient
+from actionlib_msgs.msg       import GoalStatus
 from aist_fastening_tools.msg import (ScrewToolCommandAction,
                                       ScrewToolCommandGoal)
 from aist_utility.compat      import *
@@ -16,7 +17,7 @@ class ScrewToolTest(object):
 
     def run(self):
         tool_name = 'screw_tool_m3'
-        speed     = 100
+        speed     = 1023
 
         while not rospy.is_shutdown():
             key = raw_input('[%s:%d]> ' % (tool_name, speed))
@@ -25,6 +26,8 @@ class ScrewToolTest(object):
             print('  q: quit this program')
             print('  t: tighten the screw')
             print('  l: loosen the screw')
+            print('  w: wait for tightening/loosening completed')
+            print('  c: cancel tightening/loosening')
             print('  s: set tool speed')
             print('  3: switch the tool to m3')
             print('  4: switch the tool to m4')
@@ -32,9 +35,13 @@ class ScrewToolTest(object):
             if key == 'q':
                 break
             elif key == 't':
-                self._send_command(tool_name, True, speed)
+                self._send_command(tool_name, True, True, speed)
             elif key == 'l':
-                self._send_command(tool_name, False, speed)
+                self._send_command(tool_name, False, False, speed)
+            elif key == 'w':
+                self._wait()
+            elif key == 'c':
+                self._client.cancel_goal()
             elif key == 's':
                 speed = np.clip(int(raw_input('  speed? ')), 0, 1023)
             elif key == '3':
@@ -44,15 +51,19 @@ class ScrewToolTest(object):
             else:
                 print('Unknown command[%s]' % key)
 
-    def _send_command(self, tool_name, tighten, speed):
+    def _send_command(self, tool_name, tighten, retighten, speed):
         goal = ScrewToolCommandGoal()
         goal.tool_name = tool_name
-        goal.speed     = speed
         goal.tighten   = tighten
-        self._client.send_goal_and_wait(goal,
-                                        rospy.Duration(10), rospy.Duration(10))
-        return self._client.get_result()
+        goal.retighten = retighten
+        goal.speed     = speed
+        self._client.send_goal(goal)
 
+    def _wait(self, timeout=rospy.Duration(10)):
+        self._client.wait_for_result(timeout)
+        status = self._client.get_state()
+        if status == GoalStatus.SUCCEEDED:
+            print("  succeeded")
 
 if __name__ == '__main__':
     try:
