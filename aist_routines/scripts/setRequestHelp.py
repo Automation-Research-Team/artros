4c133a36b4f683e813403dac295b8b59c727a928
Toshio UESHBIA <t.ueshiba@aist.go.jp>
2023-10-19 04:19:45 +0900

    Add camera_bridge.py which subscribes images and publish them toward NEP

--- aist_routines/scripts/setRequestHelp.py
+++ aist_routines/scripts/setRequestHelp.py
@@ -7,11 +7,11 @@ from finger_pointing_msgs.msg import request_help
 # 		the IP address of PC running NEP CLI
 IP = '163.220.51.108'
 # Create a new nep node
-node = nep.node("setRequestHelp")
+node = nep.node('setRequestHelp')
 conf = node.hybrid(IP)
 # Create a new nep subscriber with the topic <'test'>
-pubRequestHelp = node.new_pub('VR/RequestHelp', "json", conf)
-rospy.init_node("setRequestHelp", anonymous=True)
+pubRequestHelp = node.new_pub('VR/RequestHelp', 'json', conf)
+rospy.init_node('setRequestHelp', anonymous=True)
 
 
 def callback_RequestHelp(data):
@@ -20,22 +20,22 @@ def callback_RequestHelp(data):
     item_id = data.item_id
     pose = data.pose.pose
 
-    msg_pose = {"position":    {"x": -pose.position.y,
-                                "y":  pose.position.z,
-                                "z":  pose.position.x},
-                "orientation": {"x": -pose.orientation.x,
-                                "y":  pose.orientation.z,
-                                "z":  pose.orientation.x,
-                                "w":  pose.orientation.w }}
+    msg_pose = {'position':    {'x': -pose.position.y,
+                                'y':  pose.position.z,
+                                'z':  pose.position.x},
+                'orientation': {'x': -pose.orientation.x,
+                                'y':  pose.orientation.z,
+                                'z':  pose.orientation.x,
+                                'w':  pose.orientation.w }}
     request = data.request
     message = data.message
-    pubRequestHelp.publish({"robot_name": robot_name,
-                            "item_id":    item_id,
-                            "pose":       msg_pose,
+    pubRequestHelp.publish({'robot_name': robot_name,
+                            'item_id':    item_id,
+                            'pose':       msg_pose,
                             'request':    request,
                             'message':    message})
 
  ## Here write the ROS subscriber code ...
-topic_name = "help"
+topic_name = 'help'
 rospy.Subscriber(topic_name, request_help, callback_RequestHelp)
 rospy.spin()
